695a81ac2e3ac1ad5938a4b725acbc317eb3b8b1
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-05-03 12:57:25 +0300

    fix: cleanup schema.sql

--- MVP/backend/app/sql/schema.sql
+++ MVP/backend/app/sql/schema.sql
@@ -20,30 +20,4 @@ CREATE TABLE IF NOT EXISTS "users" (
     "deleted_at" TIMESTAMP,
 
     PRIMARY KEY("id")
-);CREATE TABLE IF NOT EXISTS "users" (
-    "id" UUID,
-    "name" VARCHAR(255),
-    "email" VARCHAR(255) NOT NULL UNIQUE,
-    "password_hash" VARCHAR(255), -- Stored as a bcrypt hash
-    "system_role" VARCHAR(50), -- Expected values: 'admin', 'customer', 'artist'
-    "fcm_token" VARCHAR(255), -- Firebase Cloud Messaging token
-    "is_active" BOOLEAN DEFAULT true
-    "created_at" TIMESTAMP DEFAULT now(),
-    "updated_at" TIMESTAMP,
-    "deleted_at" TIMESTAMP,
-
-    PRIMARY KEY("id")
-);CREATE TABLE IF NOT EXISTS "users" (
-    "id" UUID,
-    "name" VARCHAR(255),
-    "email" VARCHAR(255) NOT NULL UNIQUE,
-    "password_hash" VARCHAR(255), -- Stored as a bcrypt hash
-    "system_role" VARCHAR(50), -- Expected values: 'admin', 'customer', 'artist'
-    "fcm_token" VARCHAR(255), -- Firebase Cloud Messaging token
-    "is_active" BOOLEAN DEFAULT true,
-    "created_at" TIMESTAMP DEFAULT now(),
-    "updated_at" TIMESTAMP,
-    "deleted_at" TIMESTAMP,
-
-    PRIMARY KEY("id")
 );
