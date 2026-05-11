08b449713238c9ea65bb9b69714734e683f2804b
Lamyaa439 <lamyaaalghaihab@gmail.com>
2026-05-11 16:23:17 +0300

    Update with get_user_by_fcm_token and update_fcm_token

--- MVP/backend/app/persistence/repositories/user_repo.py
+++ MVP/backend/app/persistence/repositories/user_repo.py
@@ -20,7 +20,18 @@ class UserRepository(SQLAlchemyRepository):
         normalized_email = email.strip().lower()
         return self.model.query.filter_by(email=normalized_email).first()
     
+    def update_fcm_token(self, user_id, new_token):
+        """
+        Updates the Firebase Cloud Messaging token for a specific user
+        """
+        return self.update(user_id, {"fcm_token": new_token})
     
+    def get_user_by_fcm_token(self, fcm_token):
+        """
+        Finds a user by their device token.
+        """
+        return self.get_by_attribute("fcm_token", fcm_token)
+
     def get_user_by_id(self, user_id):
         """
         Fetch a user by their unique ID.
