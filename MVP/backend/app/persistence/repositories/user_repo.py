0585ca15fed6e3c3b1c7390d01710446138ebf80
Lamyaa439 <lamyaaalghaihab@gmail.com>
2026-05-11 12:21:04 +0300

    feat: create specific UserRepository with email normalization logic

--- MVP/backend/app/persistence/repositories/user_repo.py
+++ MVP/backend/app/persistence/repositories/user_repo.py
@@ -3,37 +3,27 @@ User repository.
 
 Handles database operations related to the User model.
 """
-
-from app.extensions import db
+from app.persistence.repository import SQLAlchemyRepository
 from app.models.user import User
-from sqlalchemy.exc import IntegrityError
 
 
-def get_user_by_email(email: str):
-    """
-    Retrieve a user by email.
-    """
-    normalized_email = email.strip().lower() if email else None
-    return User.query.filter_by(email=normalized_email).first()
-
-
-def get_user_by_id(id):
-    """
-    Retrieve a user by ID
-    """ 
-    return 
-
-def create_user(user_data: dict):
-    """
-    Create and save a new user in the database.
-    """
-    user = User(
-        name=user_data["name"],
-        email=user_data["email"],
-        password=user_data["password"]  # model handles hashing
-    )
-
-    db.session.add(user)
-    db.session.commit()
-
-    return user
+class UserRepository(SQLAlchemyRepository):
+    def __init__(self):
+        super().__init__(User)
+        
+    def get_user_by_email(self, email):
+        """
+        Fetch a user by their email address.
+        """
+        if not email:
+            return None
+        normalized_email = email.strip().lower()
+        return self.model.query.filter_by(email=normalized_email).first()
+    
+    
+    def get_user_by_id(self, user_id):
+        """
+        Fetch a user by their unique ID.
+        Useful for the Refresh Token logic
+        """
+        return self.get(user_id)
