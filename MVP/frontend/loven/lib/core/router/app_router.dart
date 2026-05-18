d4363b4c3688afd22930cada7ba84006730466f0
Yara Alrasheed <11982@holbertonstudents.com>
2026-05-19 02:09:14 +0300

    Fix import issues

--- MVP/frontend/loven/lib/core/router/app_router.dart
+++ MVP/frontend/loven/lib/core/router/app_router.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:loven/presentation/splash/splash_screen.dart';
-import 'package:loven/presentation/auth/screens/signup_page.dart';
+import 'package:loven/features/authentication/presentation/screens/signup_page.dart';
 import 'package:loven/features/navigation/screen/navigation_screen.dart';
 
 bool isUserBrowsingAsGuest = true;
