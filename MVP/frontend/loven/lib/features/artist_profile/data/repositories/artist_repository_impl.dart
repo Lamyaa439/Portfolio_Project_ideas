1859f337d64544750aa3571473b0bf99e516d3c3
Lamyaa439 <lamyaaalghaihab@gmail.com>
2026-05-18 17:57:39 +0300

    add getMyProfile() and updateMyProfile()

--- MVP/frontend/loven/lib/features/artist_profile/data/repositories/artist_repository_impl.dart
+++ MVP/frontend/loven/lib/features/artist_profile/data/repositories/artist_repository_impl.dart
@@ -1,6 +1,7 @@
 import '../../domain/entities/artist.dart';
 import '../../domain/repositories/artist_repository.dart';
 import '../datasources/artist_remote_datasource.dart';
+import '../../../../core/storage/token_storage.dart';
 
 /// **Artist Repository Implementation**
 /// 
@@ -9,11 +10,49 @@ import '../datasources/artist_remote_datasource.dart';
 
 class ArtistRepositoryImpl implements ArtistRepository {
   final ArtistRemoteDataSource remoteDataSource;
+  final TokenStorage tokenStorage; 
 
-  ArtistRepositoryImpl({required this.remoteDataSource});
+  ArtistRepositoryImpl({
+    required this.remoteDataSource,
+    required this.tokenStorage,
+    });
 
+  // تجيب بروفايل اي فنان لنعرضه للمستخدم
   @override
   Future<Artist> getArtistById(String artistId) async {
     return await remoteDataSource.getArtistById(artistId);
+  }
+
+  // تجيب بيانات الفنان نفسه
+  @override
+  Future<Artist> getMyProfile() async {
+    final String? token = await tokenStorage.getToken();
+    if (token == null) {
+      throw Exception('Unauthorized: No token found. Please login.');
+    }
+
+    return await remoteDataSource.getMyProfile(token);
+  }
+
+  // ترسل تحديث بيانات الفنان إذا عدلها
+  @override
+  Future<Artist> updateMyProfile({
+    String? bio,
+    String? city,
+    String? shippingPolicy,
+  }) async {
+    // نتحقق من الصلاحيات 
+    final String? token = await tokenStorage.getToken();
+
+    if (token == null) {
+      throw Exception('Unauthorized: No token found. Please login.');
+    }
+
+    return await remoteDataSource.updateMyProfile(
+      token,
+      bio: bio,
+      city: city,
+      shippingPolicy: shippingPolicy,
+      );
   }
 }
