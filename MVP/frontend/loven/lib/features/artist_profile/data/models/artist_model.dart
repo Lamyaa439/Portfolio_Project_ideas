633692dfb31dd3e00312a6006496f4b63a787ba9
Lamyaa439 <lamyaaalghaihab@gmail.com>
2026-05-18 14:04:31 +0300

    feat: add toJson to ArtistModel and integrate isVerified field

--- MVP/frontend/loven/lib/features/artist_profile/data/models/artist_model.dart
+++ MVP/frontend/loven/lib/features/artist_profile/data/models/artist_model.dart
@@ -1,6 +1,10 @@
 import '../../domain/entities/artist.dart';
 import '../../domain/entities/artwork.dart';
 
+/// **Artist Model**
+/// 
+/// Data layer representation of the Artist. It extends the Domain Entity 
+/// to add JSON serialization logic.
 class ArtistModel extends Artist {
   const ArtistModel({
     required super.id,
@@ -10,9 +14,11 @@ class ArtistModel extends Artist {
     super.city,
     super.profileImageUrl,
     super.shippingPolicy,
+    super.isVerified,
     super.artworks,
   });
 
+  // object لما تجي بيانات من السيرفر نحولها إلى 
   factory ArtistModel.fromJson(Map<String, dynamic> json) {
     return ArtistModel(
       id: json['id'] as String,
@@ -22,6 +28,8 @@ class ArtistModel extends Artist {
       city: json['city'] as String?,
       profileImageUrl: json['profile_image_url'] as String?,
       shippingPolicy: json['shipping_policy'] as String?,
+      isVerified: (json['is_verified'] as bool?) ?? false,
+
       artworks: (json['artworks'] as List<dynamic>?)
               ?.map((e) => Artwork(
                     id: e['id'] as String,
@@ -33,4 +41,15 @@ class ArtistModel extends Artist {
           const [],
     );
   }
+
+  // JSON لما نرسل بيانات إلى السيرفر نحولها إلى 
+  // Map like dict in python, String: keys and dynamic: Values
+    Map<String, dynamic> toJson() {
+      return {
+        'name': displayName,
+        'bio': bio,
+        'city': city,
+        'shipping_policy':shippingPolicy,
+      };
+    }
 }
